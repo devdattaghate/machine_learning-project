2e0f8f1b78e1d4af1275dedd4e5fbd6591d88d3c
devdattaghate <devdattaghate91@gmail.com>
2024-08-08 16:59:13 +0530

    complete pipeline has been established & updated

--- housing/config/configuration.py
+++ housing/config/configuration.py
@@ -69,7 +69,14 @@ class Configuration:
 
 
     def get_data_validation_config(self) -> DataValidationConfig:
-        pass
+        try:
+             
+            data_validation_config = DataValidationConfig(
+                schema_file_path=schema_file_path
+            )
+            return data_validation_config
+        except Exception as e:
+            raise HousingException(e,sys) from e
 
     def get_data_transformation_config(self) -> DataTransformationConfig:
         pass
